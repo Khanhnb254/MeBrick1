6c6eef2a419fae5a0ca090bfbb68066d1895054c
Khanhnb254 <22010284@st.phenikaa-uni.edu.vn>
2026-03-31 13:13:27 +0700

    Move face-female-06 down 0.7px (offsetYExtra -1.8 -> -1.1)
    Requested: mặt nữ 2 dịch xuống 0.7px.

--- kakaa/f/app/design/config/lego.config.js
+++ kakaa/f/app/design/config/lego.config.js
@@ -367,7 +367,7 @@ export const LEGO_CONFIG = {
       part: "head",
       price: 0,
       layerType: "face",
-      offsetYExtra: -1.8,
+      offsetYExtra: -1.1,
     },
     {
       id: "face-female-10",
